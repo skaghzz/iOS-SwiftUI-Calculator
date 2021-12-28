ef108a08fce5b2bc72f5877717ad6fa0071eebac
namho kim <skaghzz@naver.com>
2021-12-28 21:56:07 +0900

    Project setup

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by dev on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
